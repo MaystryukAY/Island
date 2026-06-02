cd56a31f4c7625640c026d7a6452c27a4d05c6eb
MaystryukAY <175186792+MaystryukAY@users.noreply.github.com>
2026-06-02 09:53:02 +0500

    2

--- src/main/java/ru/javarush/maystryuk/island/Main.java
+++ src/main/java/ru/javarush/maystryuk/island/Main.java
@@ -2,6 +2,6 @@ package ru.javarush.maystryuk.island;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello2!");
+        System.out.println("hello44!");
     }
 }
